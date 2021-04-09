0d2e6ce8b3b42ada1e9de7615a330ebfedd59e1d
Mark Moxon <mark@moxon.net>
2021-04-09 15:00:53 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,31 @@
+BEEBASM?=beebasm
+PYTHON?=python
+
+rel-electron=1
+folder-electron=''
+
+.PHONY:build
+build:
+	echo _VERSION=1 > sources/elite-header.h.asm
+	echo _RELEASE=$(rel-electron) >> sources/elite-header.h.asm
+	echo _REMOVE_CHECKSUMS=TRUE >> sources/elite-header.h.asm
+	#$(BEEBASM) -i sources/elite-source.asm -v > output/compile.txt
+	#$(BEEBASM) -i sources/elite-bcfs.asm -v >> output/compile.txt
+	#$(BEEBASM) -i sources/elite-loader.asm -v >> output/compile.txt
+	#$(PYTHON) sources/elite-checksum.py -u -rel$(rel-electron)
+	$(BEEBASM) -i sources/elite-disc.asm -do elite-electron.ssd -opt 3
+
+.PHONY:encrypt
+encrypt:
+	echo _VERSION=1 > sources/elite-header.h.asm
+	echo _RELEASE=$(rel-electron) >> sources/elite-header.h.asm
+	echo _REMOVE_CHECKSUMS=FALSE >> sources/elite-header.h.asm
+	#$(BEEBASM) -i sources/elite-source.asm -v > output/compile.txt
+	#$(BEEBASM) -i sources/elite-bcfs.asm -v >> output/compile.txt
+	#$(BEEBASM) -i sources/elite-loader.asm -v >> output/compile.txt
+	#$(PYTHON) sources/elite-checksum.py -rel$(rel-electron)
+	$(BEEBASM) -i sources/elite-disc.asm -do elite-electron.ssd -opt 3
+
+.PHONY:verify
+verify:
+	@$(PYTHON) sources/crc32.py extracted$(folder-electron) output
---
+++ sources/elite-disc.asm
@@ -0,0 +1,30 @@
+\ ******************************************************************************
+\
+\ ELECTRON ELITE DISC IMAGE SCRIPT
+\
+\ Electron Elite was written by Ian Bell and David Braben and is copyright
+\ Acornsoft 1984
+\
+\ The code on this site has been disassembled from the version released on Ian
+\ Bell's personal website at http://www.elitehomepage.org/
+\
+\ The commentary is copyright Mark Moxon, and any misunderstandings or mistakes
+\ in the documentation are entirely my fault
+\
+\ The terminology and notations used in this commentary are explained at
+\ https://www.bbcelite.com/about_site/terminology_used_in_this_commentary.html
+\
+\ ------------------------------------------------------------------------------
+\
+\ This source file produces the following SSD disc image:
+\
+\   * elite-electron.ssd
+\
+\ This can be loaded into an emulator or a real Electron.
+\
+\ ******************************************************************************
+
+PUTFILE "binaries/$.!BOOT.bin", "!BOOT", &0000, &0000
+PUTFILE "binaries/$.ELITE.bin", "ELITE", &FF0E00, &FF8023
+PUTFILE "extracted/$.ELITECO.bin", "ELITECO", &000000, &FFFFFF
+PUTFILE "extracted/$.ELITEDA.bin", "ELITEDA", &FF4400, &FF5200
