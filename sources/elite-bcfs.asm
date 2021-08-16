dde10820da20e8729fcff5fa5724f0111baec02f
Mark Moxon <mark@moxon.net>
2021-08-16 22:44:07 +0100

    Added GUARD directives

--- sources/elite-bcfs.asm
+++ sources/elite-bcfs.asm
@@ -39,6 +39,8 @@
 
 INCLUDE "sources/elite-header.h.asm"
 
+GUARD &5800             \ Guard against assembling over screen memory
+
 \ ******************************************************************************
 \
 \ Configuration variables
