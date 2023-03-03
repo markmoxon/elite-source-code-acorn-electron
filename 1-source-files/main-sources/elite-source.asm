34f73ec092d107ca8c4c56b83eefc86a1beaee3d
Mark Moxon <mark@moxon.net>
2023-03-03 14:27:03 +0000

    Updates to arctan and ellipse commentary

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -14757,6 +14757,11 @@ NEXT
 \
 \   tan(A) = P / Q
 \
+\ The result in A is an integer representing the angle in radians, where 256
+\ represents a full circle of 360 degrees, or 2 * PI radians. The routine
+\ returns values in the range 0 to 128, which covers 0 to 180 degrees (or 0 to
+\ PI radians).
+\
 \ ******************************************************************************
 
 .ARCTAN
@@ -14848,7 +14853,10 @@ NEXT
 
 .ARS1
 
-                        \ This routine fetches arctan(A / Q) from the ACT table
+                        \ This routine fetches arctan(A / Q) from the ACT table,
+                        \ so A will be set to an integer in the range 0 to 31
+                        \ that represents an angle from 0 to 45 degrees (or 0 to
+                        \ PI / 4 radians)
 
  JSR LL28               \ Call LL28 to calculate:
                         \
@@ -14878,26 +14886,35 @@ NEXT
 \
 \ ------------------------------------------------------------------------------
 \
-\ To calculate the following:
+\ This table contains lookup values for arctangent calculations involving angles
+\ in the range 0 to 45 degrees (or 0 to PI / 4 radians).
+\
+\ To calculate the value of theta in the following:
 \
 \   theta = arctan(t)
 \
-\ where 0 <= t < 1, look up the value in:
+\ where 0 <= t < 1, we look up the value in:
 \
 \   ACT + (t * 32)
 \
-\ The result will be an integer representing the angle in radians, with 256
-\ representing a full circle of 2 * PI radians.
+\ The result will be an integer representing the angle in radians, where 256
+\ represents a full circle of 360 degrees (2 * PI radians). The result of the
+\ lookup will therefore be an integer in the range 0 to 31, as this represents
+\ 0 to 45 degrees (0 to PI / 4 radians).
 \
-\ The table does not support values of t >= 1 or t < 0 directly, but we can use
-\ the following calculations instead:
+\ The table does not support values of t >= 1 or t < 0 directly, so if we need
+\ to calculate the arctangent for an angle greater than 45 degrees, we can apply
+\ the following calculation to the result from the table:
 \
 \   * For t > 1, arctan(t) = 64 - arctan(1 / t)
 \
+\ For negative values of t where -1 < t < 0, we can apply the following
+\ calculation to the result from the table:
+\
 \   * For t < 0, arctan(-t) = 128 - arctan(t)
 \
-\ If t < -1, we can do the first one to get arctan(|t|), then the second to get
-\ arctan(-|t|).
+\ Finally, if t < -1, we can do the first calculation to get arctan(|t|), and
+\ the second to get arctan(-|t|).
 \
 \ ******************************************************************************
 
@@ -32919,7 +32936,7 @@ LOAD_G% = LOAD% + P% - CODE%
                         \ We now keep halving |delta_x| and |delta_y| until
                         \ both of them have zero in their high bytes
 
- TAX                    \ IF |delta_x_hi| is non-zero, skip the following
+ TAX                    \ If |delta_x_hi| is non-zero, skip the following
  BNE LL112
 
  LDX XX12+5             \ If |delta_y_hi| = 0, jump down to LL113 (as both
