385e09469cbe1e5ea3f06490779744c35b3a8d8b
Mark Moxon <mark@moxon.net>
2023-06-28 13:05:36 +0100

    Extract the EOR value for recursive text tokens into a configuration variable

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -81,6 +81,9 @@
  func9 = &A6            \ Internal key number for FUNC-9 (Status Mode)
  func0 = &A7            \ Internal key number for FUNC-0 (Inventory)
 
+ RE = &23               \ The obfuscation byte used to hide the recursive tokens
+                        \ table from crackers viewing the binary code
+
  VIA = &FE00            \ Memory-mapped space for accessing internal hardware,
                         \ such as the video ULA, 6845 CRTC and 6522 VIAs (also
                         \ known as SHEILA)
@@ -1205,9 +1208,9 @@
 MACRO CHAR x
 
  IF x = '`'
-   EQUB 39 EOR 35
+   EQUB 39 EOR RE
  ELSE
-   EQUB x EOR 35
+   EQUB x EOR RE
  ENDIF
 
 ENDMACRO
@@ -1241,38 +1244,38 @@ ENDMACRO
 
 MACRO TWOK t, k
 
- IF t = 'A' AND k = 'L' : EQUB 128 EOR 35 : ENDIF
- IF t = 'L' AND k = 'E' : EQUB 129 EOR 35 : ENDIF
- IF t = 'X' AND k = 'E' : EQUB 130 EOR 35 : ENDIF
- IF t = 'G' AND k = 'E' : EQUB 131 EOR 35 : ENDIF
- IF t = 'Z' AND k = 'A' : EQUB 132 EOR 35 : ENDIF
- IF t = 'C' AND k = 'E' : EQUB 133 EOR 35 : ENDIF
- IF t = 'B' AND k = 'I' : EQUB 134 EOR 35 : ENDIF
- IF t = 'S' AND k = 'O' : EQUB 135 EOR 35 : ENDIF
- IF t = 'U' AND k = 'S' : EQUB 136 EOR 35 : ENDIF
- IF t = 'E' AND k = 'S' : EQUB 137 EOR 35 : ENDIF
- IF t = 'A' AND k = 'R' : EQUB 138 EOR 35 : ENDIF
- IF t = 'M' AND k = 'A' : EQUB 139 EOR 35 : ENDIF
- IF t = 'I' AND k = 'N' : EQUB 140 EOR 35 : ENDIF
- IF t = 'D' AND k = 'I' : EQUB 141 EOR 35 : ENDIF
- IF t = 'R' AND k = 'E' : EQUB 142 EOR 35 : ENDIF
- IF t = 'A' AND k = '?' : EQUB 143 EOR 35 : ENDIF
- IF t = 'E' AND k = 'R' : EQUB 144 EOR 35 : ENDIF
- IF t = 'A' AND k = 'T' : EQUB 145 EOR 35 : ENDIF
- IF t = 'E' AND k = 'N' : EQUB 146 EOR 35 : ENDIF
- IF t = 'B' AND k = 'E' : EQUB 147 EOR 35 : ENDIF
- IF t = 'R' AND k = 'A' : EQUB 148 EOR 35 : ENDIF
- IF t = 'L' AND k = 'A' : EQUB 149 EOR 35 : ENDIF
- IF t = 'V' AND k = 'E' : EQUB 150 EOR 35 : ENDIF
- IF t = 'T' AND k = 'I' : EQUB 151 EOR 35 : ENDIF
- IF t = 'E' AND k = 'D' : EQUB 152 EOR 35 : ENDIF
- IF t = 'O' AND k = 'R' : EQUB 153 EOR 35 : ENDIF
- IF t = 'Q' AND k = 'U' : EQUB 154 EOR 35 : ENDIF
- IF t = 'A' AND k = 'N' : EQUB 155 EOR 35 : ENDIF
- IF t = 'T' AND k = 'E' : EQUB 156 EOR 35 : ENDIF
- IF t = 'I' AND k = 'S' : EQUB 157 EOR 35 : ENDIF
- IF t = 'R' AND k = 'I' : EQUB 158 EOR 35 : ENDIF
- IF t = 'O' AND k = 'N' : EQUB 159 EOR 35 : ENDIF
+ IF t = 'A' AND k = 'L' : EQUB 128 EOR RE : ENDIF
+ IF t = 'L' AND k = 'E' : EQUB 129 EOR RE : ENDIF
+ IF t = 'X' AND k = 'E' : EQUB 130 EOR RE : ENDIF
+ IF t = 'G' AND k = 'E' : EQUB 131 EOR RE : ENDIF
+ IF t = 'Z' AND k = 'A' : EQUB 132 EOR RE : ENDIF
+ IF t = 'C' AND k = 'E' : EQUB 133 EOR RE : ENDIF
+ IF t = 'B' AND k = 'I' : EQUB 134 EOR RE : ENDIF
+ IF t = 'S' AND k = 'O' : EQUB 135 EOR RE : ENDIF
+ IF t = 'U' AND k = 'S' : EQUB 136 EOR RE : ENDIF
+ IF t = 'E' AND k = 'S' : EQUB 137 EOR RE : ENDIF
+ IF t = 'A' AND k = 'R' : EQUB 138 EOR RE : ENDIF
+ IF t = 'M' AND k = 'A' : EQUB 139 EOR RE : ENDIF
+ IF t = 'I' AND k = 'N' : EQUB 140 EOR RE : ENDIF
+ IF t = 'D' AND k = 'I' : EQUB 141 EOR RE : ENDIF
+ IF t = 'R' AND k = 'E' : EQUB 142 EOR RE : ENDIF
+ IF t = 'A' AND k = '?' : EQUB 143 EOR RE : ENDIF
+ IF t = 'E' AND k = 'R' : EQUB 144 EOR RE : ENDIF
+ IF t = 'A' AND k = 'T' : EQUB 145 EOR RE : ENDIF
+ IF t = 'E' AND k = 'N' : EQUB 146 EOR RE : ENDIF
+ IF t = 'B' AND k = 'E' : EQUB 147 EOR RE : ENDIF
+ IF t = 'R' AND k = 'A' : EQUB 148 EOR RE : ENDIF
+ IF t = 'L' AND k = 'A' : EQUB 149 EOR RE : ENDIF
+ IF t = 'V' AND k = 'E' : EQUB 150 EOR RE : ENDIF
+ IF t = 'T' AND k = 'I' : EQUB 151 EOR RE : ENDIF
+ IF t = 'E' AND k = 'D' : EQUB 152 EOR RE : ENDIF
+ IF t = 'O' AND k = 'R' : EQUB 153 EOR RE : ENDIF
+ IF t = 'Q' AND k = 'U' : EQUB 154 EOR RE : ENDIF
+ IF t = 'A' AND k = 'N' : EQUB 155 EOR RE : ENDIF
+ IF t = 'T' AND k = 'E' : EQUB 156 EOR RE : ENDIF
+ IF t = 'I' AND k = 'S' : EQUB 157 EOR RE : ENDIF
+ IF t = 'R' AND k = 'I' : EQUB 158 EOR RE : ENDIF
+ IF t = 'O' AND k = 'N' : EQUB 159 EOR RE : ENDIF
 
 ENDMACRO
 
@@ -1301,7 +1304,7 @@ ENDMACRO
 
 MACRO CONT n
 
- EQUB n EOR 35
+ EQUB n EOR RE
 
 ENDMACRO
 
@@ -1345,7 +1348,7 @@ MACRO RTOK n
   t = n
  ENDIF
 
- EQUB t EOR 35
+ EQUB t EOR RE
 
 ENDMACRO
 
@@ -1361,7 +1364,7 @@ ENDMACRO
 
 .QQ18
 
- EQUB &FF EOR 35        \ Token 0 is unused in the Electron version of Elite,
+ EQUB &FF EOR RE        \ Token 0 is unused in the Electron version of Elite,
  EQUB 0                 \ and it just contains &FF (plus the standard token
                         \ obfuscation EOR) as filler
 
@@ -7894,7 +7897,7 @@ ENDIF
 \       Name: NLIN3
 \       Type: Subroutine
 \   Category: Drawing lines
-\    Summary: Print a title and a horizontal line at row 19 to box it in
+\    Summary: Print a title and draw a horizontal line at row 19 to box it in
 \
 \ ------------------------------------------------------------------------------
 \
@@ -21031,7 +21034,7 @@ ENDIF
                         \ return from the subroutine using a tail call
 
  DEX                    \ If token = 2, this is control code 2 (current system
- BEQ ypl                \ name), so jump to ypl to print the current system name 
+ BEQ ypl                \ name), so jump to ypl to print the current system name
                         \ and return from the subroutine using a tail call
 
  DEX                    \ If token > 3, skip the following instruction
@@ -21548,8 +21551,9 @@ ENDIF
                         \ which is the next character of this token that we
                         \ want to print
 
- EOR #35                \ Tokens are stored in memory having been EOR'd with 35
-                        \ (see variable QQ18 for details), so we repeat the
+ EOR #RE                \ Tokens are stored in memory having been EOR'd with the
+                        \ value of RE - which is 35 for all versions of Elite
+                        \ except for NES, where RE is 62 - so we repeat the
                         \ EOR to get the actual character to print
 
  JSR TT27               \ Print the text token in A, which could be a letter,
